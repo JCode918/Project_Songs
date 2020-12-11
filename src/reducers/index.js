e6618dc73acfc46f5c58e48dd9c5de871fae472f
Willard Joseph <Willard.Joseph26@gmail.com>
2020-12-11 14:44:33 -0500

    Actions and Reducers Created

---
+++ src/reducers/index.js
@@ -0,0 +1,34 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+	return [
+		{
+			title: 'No Scrubs',
+			duration: '4:05',
+		},
+		{
+			title: 'Macerana',
+			duration: '2:02',
+		},
+		{
+			title: 'All Star',
+			duration: '3:47',
+		},
+		{
+			title: 'September',
+			duration: '5:01',
+		},
+		{
+			title: 'El Shaddai',
+			duration: '4:44',
+		},
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === 'SONG_SELECTED') {
+		return action.payload;
+	}
+
+	return selectedSong;
+};
